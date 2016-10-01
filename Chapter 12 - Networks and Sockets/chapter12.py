0ae98cd39e7ff9d789a769869454deaec123dc2e
mpitts <mpitts7@gmail.com>
2016-10-01 14:30:40 -0500

    Updated Assignments
    Included the assignment description in files.

--- Chapter 12 - Networks and Sockets/chapter12.py
+++ Chapter 12 - Networks and Sockets/chapter12.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 
-# Understanding the Request / Response Cycle
+# Chapter 12 Assignment: Understanding the Request / Response Cycle
+
+# Exploring the HyperText Transport Protocol
+
+# You are to retrieve the following document using the HTTP protocol in a way
+# that you can examine the HTTP Response headers.
+
+# http://data.pr4e.org/intro-short.txt
+# There are three ways that you might retrieve this web page and look at the
+# response headers:
+
+# Preferred: Modify the socket1.py program to retrieve the above URL and print
+# out the headers and data. Make sure to change the code to retrieve the above
+# URL - the values are different for each URL.
+
+# Open the URL in a web browser with a developer console or FireBug and manually
+# examine the headers that are returned.
+
+# Use the telnet program as shown in lecture to retrieve the headers and content.
 
 import socket
 
